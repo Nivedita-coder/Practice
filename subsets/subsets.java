ed172c01d7ce69d0249d4539b9ad6d8360fdf9c5
Nivedita Prasad <56059682+Nivedita-coder@users.noreply.github.com>
2021-05-26 20:55:21 +0530

    Time: 1 ms (51.75%), Space: 38.9 MB (91.97%) - LeetHub

---
+++ subsets/subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    public List<List<Integer>> subsets(int[] nums) {
+        if(nums.length ==0)
+            return res;
+        
+        bt(0, new ArrayList<Integer>(), nums);
+        return res;
+        
+    }
+    
+    public void bt(int start, ArrayList<Integer> curr, int[] nums){
+        res.add(new ArrayList<Integer>(curr));
+        for(int i = start; i<nums.length;i++){
+            curr.add(nums[i]);
+            bt(i+1,curr,nums);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
